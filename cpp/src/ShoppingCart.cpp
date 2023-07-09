5ddb52a2d52aab322135ccbc9dc595273364c26c
Amir-KN <amirhoseinn1412@gmail.com>
2023-07-09 19:53:34 +0330

    Use value instead of hard code value(5)

--- cpp/src/ShoppingCart.cpp
+++ cpp/src/ShoppingCart.cpp
@@ -38,8 +38,8 @@ void ShoppingCart::handleOffers(Receipt& receipt, std::map<Product, Offer> offer
                 boughtProductsNum = 3;
             } else if (offer.getOfferType() == SpecialOfferType::TwoForAmount) {
                 boughtProductsNum = 2;
-                if (quantityAsInt >= 2) {
-                    double total = offer.getArgument() * (quantityAsInt / boughtProductsNum) + quantityAsInt % 2 * unitPrice;
+                if (quantityAsInt >= boughtProductsNum) {
+                    double total = offer.getArgument() * (quantityAsInt / boughtProductsNum) + quantityAsInt % boughtProductsNum * unitPrice;
                     double discountN = unitPrice * quantity - total;
                     discount = new Discount("2 for " + std::to_string(offer.getArgument()), -discountN, product);
                 }
@@ -47,15 +47,15 @@ void ShoppingCart::handleOffers(Receipt& receipt, std::map<Product, Offer> offer
                 boughtProductsNum = 5;
             }
             int boughtProductsRatio = quantityAsInt / boughtProductsNum;
-            if (offer.getOfferType() == SpecialOfferType::ThreeForTwo && quantityAsInt > 2) {
-                double discountAmount = quantity * unitPrice - ((boughtProductsRatio * 2 * unitPrice) + quantityAsInt % 3 * unitPrice);
+            if (offer.getOfferType() == SpecialOfferType::ThreeForTwo && quantityAsInt > boughtProductsNum) {
+                double discountAmount = quantity * unitPrice - ((boughtProductsRatio * boughtProductsNum * unitPrice) + quantityAsInt % 3 * unitPrice);
                 discount = new Discount("3 for 2", -discountAmount, product);
             }
             if (offer.getOfferType() == SpecialOfferType::TenPercentDiscount) {
                 discount = new Discount(std::to_string(offer.getArgument()) + "% off", -quantity * unitPrice * offer.getArgument() / 100.0, product);
             }
-            if (offer.getOfferType() == SpecialOfferType::FiveForAmount && quantityAsInt >= 5) {
-                double discountTotal = unitPrice * quantity - (offer.getArgument() * boughtProductsRatio + quantityAsInt % 5 * unitPrice);
+            if (offer.getOfferType() == SpecialOfferType::FiveForAmount && quantityAsInt >= boughtProductsNum) {
+                double discountTotal = unitPrice * quantity - (offer.getArgument() * boughtProductsRatio + quantityAsInt % boughtProductsNum * unitPrice);
                 discount = new Discount(std::to_string(boughtProductsNum) + " for " + std::to_string(offer.getArgument()), -discountTotal, product);
             }
             if (discount != nullptr)
